69d36887711ad38d7dc60916dc4c5daa8dfc5df4
Maurício Pimenta <mjbpimenta@gmail.com>
2020-03-02 02:43:05 -0300

    Adding session box.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pomodoro_clock_flutter_mobx/action_buttons.dart';
 import 'package:pomodoro_clock_flutter_mobx/clock_core_view.dart';
+import 'package:pomodoro_clock_flutter_mobx/seesion_box.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -22,6 +23,7 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           StatusAndClockView(),
+          SessionBox(),
           ActionsButtons(),
         ],
       ),
