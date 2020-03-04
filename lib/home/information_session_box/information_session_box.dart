15f0b9abed9f5007bb8189f85a6f6c3918abbce3
Maurício Pimenta <mjbpimenta@gmail.com>
2020-03-03 22:22:59 -0300

    Uncoupling pomodoro status text.

--- lib/home/information_session_box/information_session_box.dart
+++ lib/home/information_session_box/information_session_box.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pomodoro_clock_flutter_mobx/home/information_session_box/countdown_text.dart';
-import 'package:pomodoro_clock_flutter_mobx/pomodoro/pomodoro_status.dart';
+import 'package:pomodoro_clock_flutter_mobx/home/information_session_box/status_text.dart';
 import 'package:pomodoro_clock_flutter_mobx/pomodoro/pomodoro_technique.dart';
 
 class InformationSessionBox extends StatelessWidget {
@@ -11,29 +10,8 @@ class InformationSessionBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Column(
         children: <Widget>[
-          _statusText(),
+          PomodoroStatusText(),
           CountDownText(),
         ],
       );
-
-  _statusText() => Observer(builder: (_) => _whichStatusText());
-
-  _whichStatusText() {
-    final style = TextStyle(color: Colors.grey[800], fontSize: 35);
-
-    final status = _pomodoroTechnique.pomodoro.status;
-
-    if (status == PomodoroStatus.initial) {
-      return Text("It's ready?", style: style);
-    }
-
-    if (status == PomodoroStatus.paused) {
-      return Text("It's paused!", style: style);
-    }
-
-    if (status == PomodoroStatus.started) {
-      return Text('');
-    }
-    return Text('');
-  }
 }
