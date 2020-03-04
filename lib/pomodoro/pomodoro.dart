ed0f103dea1b5386131e98027baf300978b2a4e1
Maurício Pimenta <mjbpimenta@gmail.com>
2020-03-03 21:09:11 -0300

    Abstracting pomodoro.

---
+++ lib/pomodoro/pomodoro.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:pomodoro_clock_flutter_mobx/pomodoro/pomodoro_status.dart';
+
+class Pomodoro {
+  int sessionMinutes;
+
+  int longBreakMinutes;
+
+  int shortBreakMinutes;
+
+  int sessionNumber;
+
+  PomodoroStatus status;
+
+  static final defaultConfig = Pomodoro(
+    sessionMinutes: 25,
+    longBreakMinutes: 25,
+    shortBreakMinutes: 5,
+    sessionNumber: 1,
+    status: PomodoroStatus.initial,
+  );
+
+  Pomodoro({
+    @required this.sessionMinutes,
+    @required this.longBreakMinutes,
+    @required this.shortBreakMinutes,
+    @required this.sessionNumber,
+    @required this.status,
+  });
+}
