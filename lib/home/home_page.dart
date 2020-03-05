13d529ca1a617f19613fb5f603ee70e669de8120
Maurício Pimenta <mjbpimenta@gmail.com>
2020-03-05 20:58:24 -0300

    Refactoring names.

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pomodoro_clock_flutter_mobx/buttons/action_buttons.dart';
 import 'package:pomodoro_clock_flutter_mobx/countdown/pomodoro_countdown.dart';
-import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_box/long_break_input.dart';
-import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_box/session_input.dart';
-import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_box/short_break_input.dart';
+import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_section/long_break_input.dart';
+import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_section/session_input.dart';
+import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_section/short_break_input.dart';
 import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_information_box/information_session_box.dart';
 
 class HomePage extends StatelessWidget {
--- lib/home/pomodoro_configuration_section/long_break_input.dart
+++ lib/home/pomodoro_configuration_section/long_break_input.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_box/pomodoro_configuration_text_field.dart';
+import 'package:pomodoro_clock_flutter_mobx/home/pomodoro_configuration_section/pomodoro_configuration_text_field.dart';
 
 class LongBreakBox extends StatelessWidget {
   final double buttonSize = 20;
